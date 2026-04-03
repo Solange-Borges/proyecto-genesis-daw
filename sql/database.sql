fae32be1b0a52f90edaa0eb15e85a0028c4e9d16
Solange-Borges <solangeborges82@hotmail.com>
2026-04-03 11:29:04 +0200

    Update database.sql
    Mejora y optimización de datos

--- sql/database.sql
+++ sql/database.sql
@@ -2,19 +2,16 @@ CREATE TABLE recursos (
     id INT PRIMARY KEY AUTO_INCREMENT,
     titulo VARCHAR(100),
     descripcion TEXT,
-    tipo VARCHAR(50),
     edad_min INT,
     edad_max INT,
     imagen VARCHAR(255),
-    archivo_url VARCHAR(255)
+    archivo_url VARCHAR(255),
+    id_tipo INT,
+    FOREIGN KEY (id_tipo) REFERENCES tipos(id)
 );
 
 CREATE TABLE tipos (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(50)
 );
-ALTER TABLE recursos 
-ADD id_tipo INT;
 
-ALTER TABLE recursos 
-ADD FOREIGN KEY (id_tipo) REFERENCES tipos(id);
